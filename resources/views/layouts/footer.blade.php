63e9c55b57a7f95b0bc69c395263e8c29b8b72ae
Rlocmelis <47271031+Rlocmelis@users.noreply.github.com>
2020-06-20 16:50:00 +0300

    First commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Our store:
+    <a href="">E-Shop.gg</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
